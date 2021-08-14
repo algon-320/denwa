6c97465daa5423eb5a9b2ad8dfe9af627381a0c6
algon-320 <algon.0320@gmail.com>
2021-08-15 04:01:58 +0900

    Make traffic display human-friendly

--- src/traffic_meter.rs
+++ src/traffic_meter.rs
@@ -1,3 +1,4 @@
+use std::fmt::{self, Display, Formatter};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::Instant;
 
@@ -25,18 +26,45 @@ impl TrafficMeter {
     }
 }
 
-impl std::fmt::Display for TrafficMeter {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let elapsed = self.start.elapsed().as_secs();
-        let upload = self.upload.load(Ordering::Relaxed);
-        let download = self.download.load(Ordering::Relaxed);
+impl Display for TrafficMeter {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let elapsed = self.start.elapsed().as_secs() as f64;
+        let up = self.upload.load(Ordering::Relaxed) as f64;
+        let dw = self.download.load(Ordering::Relaxed) as f64;
+        let up_bps = DataAmount((up * 8.0) / elapsed, "bps");
+        let dw_bps = DataAmount((dw * 8.0) / elapsed, "bps");
+        let up_amo = DataAmount(up, "iB");
+        let dw_amo = DataAmount(dw, "iB");
         write!(
             f,
-            "up: {:.3} Kbps (total: {} KiB) / down: {:.3} Kbps (total: {} KiB)",
-            (upload * 8) as f32 / elapsed as f32 / 1024.0,
-            upload / 1024,
-            (download * 8) as f32 / elapsed as f32 / 1024.0,
-            download / 1024,
+            "up: {} (total: {}) / down: {} (total: {})",
+            up_bps, up_amo, dw_bps, dw_amo,
         )
     }
+}
+
+struct DataAmount(f64, &'static str);
+
+impl Display for DataAmount {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let (amo, unit) = make_human_friendly(self.0);
+        write!(f, "{:.3} {}{}", amo, unit, self.1)
+    }
+}
+
+fn make_human_friendly(amount: f64) -> (f64, &'static str) {
+    let table = [
+        (0u64, ""),
+        (1 << 10, "K"),
+        (1 << 20, "M"),
+        (1 << 40, "G"),
+        (1 << 60, "T"),
+    ];
+    for (th, unit) in table.iter().copied().rev() {
+        let th = th as f64;
+        if amount >= th {
+            return (amount / th, unit);
+        }
+    }
+    unreachable!()
 }
